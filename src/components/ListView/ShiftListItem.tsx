d879ef78799118aba90d8edd38375ecadf73a962
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2023-08-19 16:56:48 +0300

    list item onpress styles

--- src/components/ListView/ShiftListItem.tsx
+++ src/components/ListView/ShiftListItem.tsx
@@ -1,18 +1,19 @@
 import React, { memo, useMemo, useState } from 'react';
-import { Text, TouchableOpacity, View, Alert } from 'react-native';
+import { Text, View, Alert } from 'react-native';
 import { ShiftEntryInState } from '../../types/shifts';
 import { useShiftStore } from '../../zustand/shiftStore';
 import { ShiftStackParamList } from '../../types/views';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { useNavigation } from '@react-navigation/native';
+import { HStack, Pressable, VStack } from 'native-base';
 
 type Props = {
   entry: ShiftEntryInState;
   idx: number;
 };
 
-const evenBgColor = 'rgba(0,0,0,0.08)';
-const oddBgColor = 'rgba(0,0,0,0)';
+const evenBgOpacity = 0.08;
+const oddBgOpacity = 0;
 
 const ShiftListItem = memo(({ entry, idx }: Props) => {
   const navigation =
@@ -79,55 +80,62 @@ const ShiftListItem = memo(({ entry, idx }: Props) => {
   );
 
   return (
-    <View
-      key={entry.id}
-      style={{
-        paddingVertical: expanded ? 15 : 10,
-        paddingHorizontal: 15,
-        backgroundColor: idx % 2 === 0 ? evenBgColor : oddBgColor,
-      }}>
-      <TouchableOpacity onPress={onPress} onLongPress={onHold}>
-        <View style={{ flex: 1, flexDirection: 'row' }}>
-          <View style={{ flex: 1, alignItems: 'center' }}>
-            <Text>
-              {startDateObj.toLocaleTimeString('en-GB', {
-                hour: '2-digit',
-                minute: '2-digit',
-              })}
-            </Text>
-            <Text>{startDateObj.toLocaleDateString('en-GB')}</Text>
-          </View>
-          <View style={{ flex: 1, alignItems: 'center' }}>
-            <Text>
-              {endDateObj.toLocaleTimeString('en-GB', {
-                hour: '2-digit',
-                minute: '2-digit',
-              })}
-            </Text>
-            <Text>{endDateObj.toLocaleDateString('en-GB')}</Text>
-          </View>
-          <View>
-            <Text>
-              {difference.hours}h {difference.minutes}m
-            </Text>
-          </View>
-        </View>
-
-        {expanded && (
-          <View style={{ marginTop: 5 }}>
-            <View>
-              <Text>id: {entry.id}</Text>
-            </View>
-            <View>
-              <Text>break: {entry.breakDuration}</Text>
-            </View>
-            <View>
-              <Text>description: {entry.description}</Text>
-            </View>
-          </View>
-        )}
-      </TouchableOpacity>
-    </View>
+    <Pressable key={entry.id} style={{}} onPress={onPress} onLongPress={onHold}>
+      {({ isPressed }) => {
+        const opacityModifier = isPressed ? 0.15 : 0;
+        return (
+          <VStack
+            style={{
+              paddingVertical: expanded ? 15 : 10,
+              paddingHorizontal: 15,
+              backgroundColor:
+                idx % 2 === 0
+                  ? `rgba(0,0,0,${evenBgOpacity + opacityModifier})`
+                  : `rgba(0,0,0,${oddBgOpacity + opacityModifier})`,
+              opacity: isPressed ? 1 : 0.9,
+            }}>
+            <HStack style={{ flex: 1, flexDirection: 'row' }}>
+              <VStack style={{ flex: 1, alignItems: 'center' }}>
+                <Text>
+                  {startDateObj.toLocaleTimeString('en-GB', {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                  })}
+                </Text>
+                <Text>{startDateObj.toLocaleDateString('en-GB')}</Text>
+              </VStack>
+              <VStack style={{ flex: 1, alignItems: 'center' }}>
+                <Text>
+                  {endDateObj.toLocaleTimeString('en-GB', {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                  })}
+                </Text>
+                <Text>{endDateObj.toLocaleDateString('en-GB')}</Text>
+              </VStack>
+              <VStack>
+                <Text>
+                  {difference.hours}h {difference.minutes}m
+                </Text>
+              </VStack>
+            </HStack>
+            {expanded && (
+              <VStack style={{ marginTop: 5 }}>
+                <View>
+                  <Text>id: {entry.id}</Text>
+                </View>
+                <View>
+                  <Text>break: {entry.breakDuration}</Text>
+                </View>
+                <View>
+                  <Text>description: {entry.description}</Text>
+                </View>
+              </VStack>
+            )}
+          </VStack>
+        );
+      }}
+    </Pressable>
   );
 });
 
